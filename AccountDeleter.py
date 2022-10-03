84e6868744d71d46f15a994985cce56c7ee6891c
Fyoodor Dorotoro <99530256+dorotorothequickend@users.noreply.github.com>
2022-10-03 21:24:12 +0300

    Update AccountDeleter.py

--- AccountDeleter.py
+++ AccountDeleter.py
@@ -18,12 +18,12 @@
 #             https://www.gnu.org/licenses/agpl-3.0.html
 #
 #
-# Special Thanks: sk1llzmeow, amore, den4iksop - they help me with set profile picture (SOON)
 # meta developer: @DorotoroMods
 
-import time
-from telethon import functions, types
 from .. import loader, utils
+import os
+import time
+from telethon import functions
 from telethon.tl.functions.account import UpdateProfileRequest
 
 @loader.tds
@@ -32,7 +32,7 @@ class AccountDeleter(loader.Module):
 
     @loader.command()
     async def delacc(self, m):
-        "- удаляет ваш аккаунт Telegram. !!!ОПАСНО!!!"
+        "- удаляет ваш аккаунт !!!ОПАСНО!!!."
         text = "Удаление аккаунта через..."
         await utils.answer(m, f"{text} <b>10</b> <emoji document_id=5296432770392791386>✈️</emoji>")
         time.sleep(0.5)
@@ -42,5 +42,10 @@ class AccountDeleter(loader.Module):
         time.sleep(1)
         await utils.answer(m, f"{text} <b>1</b> <emoji document_id=5296432770392791386>✈️</emoji>")
         time.sleep(0.8)
+        photo = "https://0x0.st/oJqh.jpg"
+        photo_ = await self.client.send_file("me", photo)
+        avatar = await self.client.upload_file(await self.client.download_file(photo_, bytes))
+        await self.client(functions.photos.UploadProfilePhotoRequest(avatar))
+        await photo_.delete()
         await self._client(functions.account.UpdateProfileRequest(first_name='Deleted Account', last_name='', about='Аккаунт удалён. Вся информация на https://telegram.org/faq'))
         await utils.answer(m, "<b>Ваш аккаунт полностью удалён. <emoji document_id=6325592348529003273>😦</emoji></b>")
