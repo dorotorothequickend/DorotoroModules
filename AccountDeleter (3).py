787a473588bd5354d753542ee502d3e9ca0304c9
Fyoodor Dorotoro <99530256+dorotorothequickend@users.noreply.github.com>
2022-10-04 09:17:28 +0300

    Fixed time.sleep()

---
+++ AccountDeleter (3).py
@@ -0,0 +1,51 @@
+# ---------------------------------------------------------------------------------
+# Name: AccountDeleter
+# Description: No description
+# Author: Dorotoro
+# Commands:
+# / / / .delacc / / /
+# ---------------------------------------------------------------------------------
+#                █████████████████████████████████████████
+#                █────██────█────█────█───█────█────█────█
+#                █─██──█─██─█─██─█─██─██─██─██─█─██─█─██─█
+#                █─██──█─██─█────█─██─██─██─██─█────█─██─█
+#                █─██──█─██─█─█─██─██─██─██─██─█─█─██─██─█
+#                █────██────█─█─██────██─██────█─█─██────█
+#                █████████████████████████████████████████
+#
+#
+#                     Copyright 2022 t.me/km90h
+#             https://www.gnu.org/licenses/agpl-3.0.html
+#
+#
+# meta developer: @DorotoroMods
+
+from .. import loader, utils
+import os
+import asyncio
+from telethon import functions
+from telethon.tl.functions.account import UpdateProfileRequest
+
+@loader.tds
+class AccountDeleter(loader.Module):
+    strings = {"name": "AccountDeleter"}
+
+    @loader.command()
+    async def delacc(self, m):
+        "- удаляет ваш аккаунт (просто меняет вашу аватарку и ник)."
+        text = "Удаление аккаунта через..."
+        await utils.answer(m, f"{text} <b>10</b> <emoji document_id=5296432770392791386>✈️</emoji>")
+        asyncio.sleep(0.5)
+        await utils.answer(m, f"{text} <b>6</b> <emoji document_id=5296432770392791386>✈️</emoji>")
+        asyncio.sleep(0.7)
+        await utils.answer(m, f"{text} <b>3</b> <emoji document_id=5296432770392791386>✈️</emoji>")
+        asyncio.sleep(1)
+        await utils.answer(m, f"{text} <b>1</b> <emoji document_id=5296432770392791386>✈️</emoji>")
+        asyncio.sleep(0.8)
+        photo = "https://0x0.st/oJqh.jpg"
+        photo_ = await self.client.send_file("me", photo)
+        avatar = await self.client.upload_file(await self.client.download_file(photo_, bytes))
+        await self.client(functions.photos.UploadProfilePhotoRequest(avatar))
+        await photo_.delete()
+        await self._client(functions.account.UpdateProfileRequest(first_name='Deleted Account', last_name='', about='Аккаунт удалён. Вся информация на https://telegram.org/faq'))
+        await utils.answer(m, "<b>Ваш аккаунт полностью удалён. <emoji document_id=6325592348529003273>😦</emoji></b>")
