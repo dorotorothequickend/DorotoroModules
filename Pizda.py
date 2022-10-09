3b71e6d8f7101fd762fc7e52c9379dae7d7b1ca3
Fyoodor Dorotoro <99530256+dorotorothequickend@users.noreply.github.com>
2022-10-09 19:36:00 +0300

    Add files via upload

---
+++ Pizda.py
@@ -0,0 +1,43 @@
+# --------------------------------------------------------------------------------
+# Name: What a Word?
+# Description: No description
+# Author: Dorotoro
+# Commands:
+# / / / .waw / / /
+# ---------------------------------------------------------------------------------
+#                █████████████████████████████████████████
+#                █────██────█────█────█───█────█────█────█
+#                █─██──█─██─█─██─█─██─██─██─██─█─██─█─██─█
+#                █─██──█─██─█────█─██─██─██─██─█────█─██─█
+#                █─██──█─██─█─█─██─██─██─██─██─█─█─██─██─█
+#                █────██────█─█─██────██─██────█─█─██────█
+#                █████████████████████████████████████████
+#
+#
+#                     Copyright 2022 t.me/km90h
+#             https://www.gnu.org/licenses/agpl-3.0.html
+#
+# ---------------------------------------------------------------------------------
+#
+# meta developer: @DorotoroMods
+
+from .. import utils, loader
+
+@loader.tds
+class whataword(loader.Module):
+    """Ищет определение слова."""
+    strings = {"name": "What a Word?"}
+
+    @loader.command()
+    async def wawcmd(self, message):
+        "<слово> - ищет определение вашего слова."
+        args = utils.get_args_raw(message)
+        ktoetochitaetlox = await utils.answer(message, '<b><i>Открываю свой словарик...</i></b>')
+        if not args: return
+        async with self._client.conversation("@definerBot") as conv:
+            perdunmsg = await conv.send_message(args)
+            jdun = await conv.get_response()
+            await perdunmsg.delete()
+            await jdun.delete()
+        text = f"<b>Я нашел кое-что в словаре:</b>\n{jdun.text}"
+        await utils.answer(ktoetochitaetlox, text)
