dfde57373f32f4e77655f98db95d16ff9b748e2c
Fyoodor Dorotoro <99530256+dorotorothequickend@users.noreply.github.com>
2022-11-10 10:42:11 +0300

    Add files via upload

---
+++ 01code.py
@@ -0,0 +1,47 @@
+#                █████████████████████████████████████████
+#                █────██────█────█────█───█────█────█────█
+#                █─██──█─██─█─██─█─██─██─██─██─█─██─█─██─█
+#                █─██──█─██─█────█─██─██─██─██─█────█─██─█
+#                █─██──█─██─█─█─██─██─██─██─██─█─█─██─██─█
+#                █────██────█─█─██────██─██────█─█─██────█
+#                █████████████████████████████████████████
+#
+#
+#                     Copyright 2022 t.me/km90h
+#             https://www.gnu.org/licenses/agpl-3.0.html
+#
+#
+# meta developer: @DorotoroMods
+# meta banner: https://0x0.st/o6qP.gif.mp4
+
+from ..  import loader, utils
+# https://clck.ru/32dhcu (StackOverflow)
+
+@loader.tds
+class tocodedecodemod(loader.Module):
+    """Ваш персональный шифратор в двоичный код."""
+    strings = {"name": "01code"}
+
+
+    @loader.command()
+    async def codeit(self, message):
+        "<текст, который необходимо зашифровать> - шифрует ваш текст в двоичный код."
+        args = utils.get_args_raw(message)
+        def to_bits(args, encoding='utf-8', errors='surrogatepass'):
+            bits = bin(int.from_bytes(args.encode(encoding, errors), 'big'))[2:]
+            return bits.zfill(8 * ((len(bits) + 7) // 8))
+        result = to_bits(args)
+        await utils.answer(message, f"<emoji document_id=4985930888572306287>🖥</emoji> <b>Текст зашифрован:</b>\n<code>{result}</code>")
+
+    @loader.command()
+    async def decode(self, message):
+        "<текст, который необходимо дешифровать> - дешифрует ваш текст в двоичный код."
+        args = utils.get_args_raw(message)
+        if not args:
+            await utils.answer(message, "<emoji document_id=4985545282113503960>🖥</emoji> <b>Что декодировать то?</b>")
+            return
+        def from_bits(bits, encoding='utf-8', errors='surrogatepass'):
+            n = int(bits, 2)
+            return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
+        result = from_bits(args)
+        await utils.answer(message, f"<emoji document_id=4985930888572306287>🖥</emoji> <b>Результат дешифровки:</b>\n<code>{result}</code>")
