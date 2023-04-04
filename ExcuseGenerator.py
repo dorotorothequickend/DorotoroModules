d443aaae6b794f222ae4703f888210a2d5c11b65
Fyoodor Dorotoro <99530256+dorotorothequickend@users.noreply.github.com>
2023-04-04 09:14:43 +0300

    Update ExcuseGenerator.py

--- ExcuseGenerator.py
+++ ExcuseGenerator.py
@@ -10,7 +10,7 @@
 #                     Copyright 2022 t.me/km90h
 #             https://www.gnu.org/licenses/agpl-3.0.html
 #
-# meta banner: https://0x0.st/ody0.gif.mp4
+# meta banner: https://raw.githubusercontent.com/dorotorothequickend/DorotoroModules/main/banners/DorotoroExcuseGenerator.png
 # meta developer: @DorotoroMods
 
 from .. import utils, loader
