60de226be2dc9f8fd755c3c9d0a7e6c8a00aeedc
Fyoodor Dorotoro <99530256+dorotorothequickend@users.noreply.github.com>
2023-04-04 09:15:32 +0300

    Update FoodRecipe.py

--- FoodRecipe.py
+++ FoodRecipe.py
@@ -9,7 +9,7 @@
 #
 # 		      Copyright 2022 t.me/km90h
 #             https://www.gnu.org/licenses/agpl-3.0.html 
-#
+# meta banner: https://raw.githubusercontent.com/dorotorothequickend/DorotoroModules/main/banners/DorotoroFoodRecipe.png
 # meta developer: @DorotoroMods
 
 from .. import loader, utils
