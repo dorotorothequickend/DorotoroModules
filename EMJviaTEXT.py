06f5e9b01ee4012643796eae786cca609a59502b
Fyoodor Dorotoro <99530256+dorotorothequickend@users.noreply.github.com>
2023-06-15 20:23:42 +0300

    Update EMJviaTEXT.py

--- EMJviaTEXT.py
+++ EMJviaTEXT.py
@@ -7,7 +7,7 @@
 #                █████████████████████████████████████████
 #
 #
-#                     Copyright 2022 t.me/km90h
+#                     Copyright 2022 t.me/Dorotoro
 #             https://www.gnu.org/licenses/agpl-3.0.html
 #
 # meta banner: https://raw.githubusercontent.com/dorotorothequickend/DorotoroModules/main/banners/DorotoroEMJviaTEXT.png
